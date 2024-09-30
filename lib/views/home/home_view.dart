76951390fd9240da9152f75c4ee6e61b272ec75f
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-09-30 10:31:45 +0700

    feat: task widget && refactor fab component

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -3,7 +3,8 @@ import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:todidu/extensions/space_exs.dart';
 import 'package:todidu/utils/app_colors.dart';
 import 'package:todidu/utils/app_str.dart';
-import 'package:todidu/views/home/widget/fab.dart';
+import 'package:todidu/views/home/components/fab.dart';
+import 'package:todidu/views/home/widget/task_widget.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -101,81 +102,7 @@ class _HomeViewState extends State<HomeView> {
                 itemCount: 20,
                 scrollDirection: Axis.vertical,
                 itemBuilder: (context, index) {
-                  return AnimatedContainer(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: AppColors.primaryColor.withOpacity(0.3),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(.1),
-                          offset: const Offset(
-                            0,
-                            4,
-                          ),
-                          blurRadius: 10,
-                        ),
-                      ],
-                    ),
-                    duration: const Duration(milliseconds: 600),
-                    child: ListTile(
-                      // Check Icon
-                      leading: GestureDetector(
-                        onTap: () {
-                          // checklist or unchecklist task
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(
-                            milliseconds: 600,
-                          ),
-                          decoration: BoxDecoration(
-                            color: AppColors.primaryColor,
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.grey,
-                              width: .8,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.check,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-
-                      // Task title
-                      title: const Padding(
-                        padding: const EdgeInsets.only(
-                          bottom: 5,
-                          top: 3,
-                        ),
-                        child: Text(
-                          "Done",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            decoration: TextDecoration.lineThrough,
-                          ),
-                        ),
-                      ),
-
-                      // Task description
-                      subtitle: Column(
-                        children: [
-                          Text(
-                            "Description",
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+                  return const TaskWidget();
                 },
               ),
             ),
---
+++ lib/views/home/widget/task_widget.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:todidu/utils/app_colors.dart';
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: AnimatedContainer(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: AppColors.primaryColor.withOpacity(0.1),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.1),
+              offset: const Offset(
+                0,
+                4,
+              ),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        duration: const Duration(milliseconds: 600),
+        child: ListTile(
+          // Check Icon
+          leading: GestureDetector(
+            onTap: () {
+              // checklist or unchecklist task
+            },
+            child: AnimatedContainer(
+              duration: const Duration(
+                milliseconds: 600,
+              ),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: .8,
+                ),
+              ),
+              child: const Icon(
+                Icons.check,
+                color: Colors.white,
+              ),
+            ),
+          ),
+
+          // Task title
+          title: const Padding(
+            padding: const EdgeInsets.only(
+              bottom: 5,
+              top: 3,
+            ),
+            child: Text(
+              "Done",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                decoration: TextDecoration.lineThrough,
+              ),
+            ),
+          ),
+
+          // Task description
+          subtitle: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Description",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+
+              // Date of Task
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 10,
+                    top: 10,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Date",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        "SubDate",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Date of Task
+        ),
+      ),
+    );
+  }
+}
